6a52accb296cd8e57cac608a2aca527c03ee7316
SooSo1234 <baptist0@naver.com>
2023-03-03 15:36:51 +0900

    문제출력

---
+++ 소병수/print.h
@@ -0,0 +1,6 @@
+#pragma once
+#include"questions.h"
+void printquestion(Question* questions, int id);
+void changerow1(Question* questions, int id);
+void changerow2(Question* questions, int id);
+void printoptions(Question* questions, int id);
