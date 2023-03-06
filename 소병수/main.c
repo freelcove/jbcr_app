e8db40484a0fc945f963393b828e086d14c990c2
SooSo1234 <baptist0@naver.com>
2023-03-06 15:33:55 +0900

    점수 계산 추가

--- 소병수/main.c
+++ 소병수/main.c
@@ -43,11 +43,12 @@ int main()
 			faltquestions(id);
 		}
 		int a = time(NULL);
-		while (time(NULL) - a < 5);
+		while (time(NULL) - a < 2);
 		ClearScreen();
 		// 다음 문제로 넘어가기
 		i++;
 	}
+	Percentage(num_to_solve);
 	//틀린 문제 풀기
 
 	repeatquestions(questions, bool());
