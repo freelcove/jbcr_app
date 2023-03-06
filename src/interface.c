532d9288808fc1349e7e9a819362ef65ba376e81
FreeLcove <freelcove@gmail.com>
2023-03-06 12:05:51 +0900

    화면 크기, 폰트 크기 조정

--- src/interface.c
+++ src/interface.c
@@ -15,8 +15,19 @@ void SetCursorVisibility(int visible)
 	SetConsoleCursorInfo(console_handle, &cursor_info);
 }
 
+void set_console_font_size(int size)
+{
+	CONSOLE_FONT_INFOEX font_info = { 0 };
+	font_info.cbSize = sizeof(font_info);
+	GetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &font_info);
+	font_info.dwFontSize.Y = size; // set font size Y coordinate
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &font_info);
+}
 
 void InitScreen() {
+
+	//폰트 크기 바꾸기
+	set_console_font_size(25);
 
 	//콘솔 창 컬러 바꾸기 (흰 배경 검은 글자)
 	system("COLOR F0");
@@ -27,7 +38,9 @@ void InitScreen() {
 
 	//콘솔 창 크기 바꾸기
 	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-	SMALL_RECT windowSize = { 0, 0, 99, 119 };
+
+	// {시작 x좌표, 시작 y좌표, 넓이(80열), 높이 (30행)}
+	SMALL_RECT windowSize = { 0, 0, 79, 29 };
 	SetConsoleWindowInfo(console, TRUE, &windowSize);
 
 	//깜빡이는 커서 없애기.
