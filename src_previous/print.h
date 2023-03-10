bbd524ac79a6f054058f77afe16052112db3c6db
FreeLcove <freelcove@gmail.com>
2023-03-10 14:44:15 +0900

    main branch에 ANSI 모드 반영

--- src_previous/print.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "globals.h"
-
-void printquestion(ObjectiveQuestion* questions, int id);
-void changerow1();
-void changerow2();
-void printoptions(ObjectiveQuestion* questions, int id);
-int randnum();
